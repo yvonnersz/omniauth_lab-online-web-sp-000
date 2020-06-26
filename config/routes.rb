196c9b89606583292e907e8185fd8f2a9f4049a3
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-26 12:47:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get '/' => 'welcome#home'
   get '/logout' => 'sessions#destroy'
 
-  get '/app/github/callback' => 'sessions#create'
+  get '/auth/github/callback' => 'sessions#create'
 
 end
