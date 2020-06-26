91742364d5cfb8b0fc85370ffe336c03b602d468
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-26 12:15:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # Add your routes here
 
-  get '/auth/github' => 'sessions#create'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
